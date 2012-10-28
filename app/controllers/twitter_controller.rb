9e4d4c02645346e995a56419bb52790d898cc618
Ramya <ramya.victory@gmail.com>
2012-10-28 14:16:35 -0500

    addded all the files

---
+++ app/controllers/twitter_controller.rb
@@ -0,0 +1,12 @@
+class TwitterController < ApplicationController
+
+def search
+	if params[:twitter]
+	@search_term = params[:twitter]
+	client = TwitterSearch::Client.new('twitter_search')
+	@tweets = client.query(:q => "#{params[:twitter]}", :page => 10, :rpp => 25)
+	end
+end
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterSearch::Application.config.secret_token = 'deb96b5000131f176c03e47b36f901edfeeb871f0d89014b53b4b45f0c0fc829998a88f28355b9d9c6101f096e921444e777d6fa02853d599a26d74cdd97f950'
